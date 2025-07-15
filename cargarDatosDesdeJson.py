820c81e50a99ad737fa260fbf9b043443b66e787
Maria del Pilar Anton <mdpanton@tsc.com.pe>
2025-07-15 13:52:29 -0500

    Agregar estructura inicial de función cargarDatosDesdeJson

---
+++ cargarDatosDesdeJson.py
@@ -0,0 +1,6 @@
+import json
+from datetime import datetime
+from clinica import SistemaCitas, Paciente, Medico, Administrador, Recepcionista, Cita, HistorialClinico
+
+def cargarDatosDesdeJson(ruta_json: str, sistema: SistemaCitas):
+    pass
